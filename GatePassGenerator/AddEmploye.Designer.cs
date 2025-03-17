512be001fb254b1535a400c4e9194e0934e0dcbd
AlexandruGr <alex.gritcan@ecap.ro>
2025-03-17 22:23:15 +0200

    .

--- GatePassGenerator/AddEmploye.Designer.cs
+++ GatePassGenerator/AddEmploye.Designer.cs
@@ -56,17 +56,19 @@ namespace GatePassGenerator
             // txtName
             // 
             this.txtName.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.txtName.Location = new System.Drawing.Point(160, 146);
+            this.txtName.Location = new System.Drawing.Point(240, 225);
+            this.txtName.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtName.Name = "txtName";
-            this.txtName.Size = new System.Drawing.Size(397, 27);
+            this.txtName.Size = new System.Drawing.Size(594, 37);
             this.txtName.TabIndex = 7;
             // 
             // btnExit
             // 
             this.btnExit.Image = ((System.Drawing.Image)(resources.GetObject("btnExit.Image")));
-            this.btnExit.Location = new System.Drawing.Point(1260, 23);
+            this.btnExit.Location = new System.Drawing.Point(1890, 35);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(45, 42);
+            this.btnExit.Size = new System.Drawing.Size(68, 65);
             this.btnExit.TabIndex = 6;
             this.btnExit.UseVisualStyleBackColor = true;
             this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
@@ -75,9 +77,10 @@ namespace GatePassGenerator
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(79, 149);
+            this.label2.Location = new System.Drawing.Point(118, 229);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(47, 19);
+            this.label2.Size = new System.Drawing.Size(72, 29);
             this.label2.TabIndex = 5;
             this.label2.Text = "Name";
             // 
@@ -86,9 +89,10 @@ namespace GatePassGenerator
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(118)))), ((int)(((byte)(225)))));
-            this.label1.Location = new System.Drawing.Point(614, 43);
+            this.label1.Location = new System.Drawing.Point(921, 66);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(208, 39);
+            this.label1.Size = new System.Drawing.Size(313, 59);
             this.label1.TabIndex = 4;
             this.label1.Text = "Add Employee";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -101,9 +105,10 @@ namespace GatePassGenerator
             this.btnSave.ForeColor = System.Drawing.Color.White;
             this.btnSave.Image = ((System.Drawing.Image)(resources.GetObject("btnSave.Image")));
             this.btnSave.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnSave.Location = new System.Drawing.Point(1214, 480);
+            this.btnSave.Location = new System.Drawing.Point(1821, 738);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(118, 44);
+            this.btnSave.Size = new System.Drawing.Size(177, 68);
             this.btnSave.TabIndex = 8;
             this.btnSave.Text = "Save";
             this.btnSave.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -113,11 +118,13 @@ namespace GatePassGenerator
             // txtHireDate
             // 
             this.txtHireDate.CalendarFont = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.txtHireDate.CustomFormat = "";
+            this.txtHireDate.CustomFormat = "yyyy-MM-dd";
             this.txtHireDate.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.txtHireDate.Location = new System.Drawing.Point(160, 199);
+            this.txtHireDate.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.txtHireDate.Location = new System.Drawing.Point(240, 306);
+            this.txtHireDate.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtHireDate.Name = "txtHireDate";
-            this.txtHireDate.Size = new System.Drawing.Size(397, 26);
+            this.txtHireDate.Size = new System.Drawing.Size(594, 35);
             this.txtHireDate.TabIndex = 9;
             this.txtHireDate.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
             // 
@@ -125,18 +132,20 @@ namespace GatePassGenerator
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label3.Location = new System.Drawing.Point(79, 204);
+            this.label3.Location = new System.Drawing.Point(118, 314);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(71, 19);
+            this.label3.Size = new System.Drawing.Size(106, 29);
             this.label3.TabIndex = 10;
             this.label3.Text = "Hire Date";
             // 
             // txtContact
             // 
             this.txtContact.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.txtContact.Location = new System.Drawing.Point(160, 253);
+            this.txtContact.Location = new System.Drawing.Point(240, 389);
+            this.txtContact.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtContact.Name = "txtContact";
-            this.txtContact.Size = new System.Drawing.Size(397, 27);
+            this.txtContact.Size = new System.Drawing.Size(594, 37);
             this.txtContact.TabIndex = 12;
             this.txtContact.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
             // 
@@ -144,9 +153,10 @@ namespace GatePassGenerator
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label4.Location = new System.Drawing.Point(79, 256);
+            this.label4.Location = new System.Drawing.Point(118, 394);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(59, 19);
+            this.label4.Size = new System.Drawing.Size(90, 29);
             this.label4.TabIndex = 11;
             this.label4.Text = "Contact";
             // 
@@ -154,9 +164,10 @@ namespace GatePassGenerator
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label5.Location = new System.Drawing.Point(79, 316);
+            this.label5.Location = new System.Drawing.Point(118, 486);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(56, 19);
+            this.label5.Size = new System.Drawing.Size(86, 29);
             this.label5.TabIndex = 14;
             this.label5.Text = "Gender";
             // 
@@ -164,9 +175,10 @@ namespace GatePassGenerator
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label6.Location = new System.Drawing.Point(79, 369);
+            this.label6.Location = new System.Drawing.Point(118, 568);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(61, 19);
+            this.label6.Size = new System.Drawing.Size(91, 29);
             this.label6.TabIndex = 16;
             this.label6.Text = "Address";
             // 
@@ -174,9 +186,10 @@ namespace GatePassGenerator
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label7.Location = new System.Drawing.Point(733, 149);
+            this.label7.Location = new System.Drawing.Point(1100, 229);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(34, 19);
+            this.label7.Size = new System.Drawing.Size(51, 29);
             this.label7.TabIndex = 18;
             this.label7.Text = "City";
             // 
@@ -184,9 +197,10 @@ namespace GatePassGenerator
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label8.Location = new System.Drawing.Point(733, 199);
+            this.label8.Location = new System.Drawing.Point(1100, 306);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(42, 19);
+            this.label8.Size = new System.Drawing.Size(64, 29);
             this.label8.TabIndex = 20;
             this.label8.Text = "State";
             // 
@@ -194,9 +208,10 @@ namespace GatePassGenerator
             // 
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label9.Location = new System.Drawing.Point(733, 251);
+            this.label9.Location = new System.Drawing.Point(1100, 386);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(75, 19);
+            this.label9.Size = new System.Drawing.Size(113, 29);
             this.label9.TabIndex = 22;
             this.label9.Text = "Username";
             // 
@@ -204,50 +219,56 @@ namespace GatePassGenerator
             // 
             this.label10.AutoSize = true;
             this.label10.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label10.Location = new System.Drawing.Point(733, 311);
+            this.label10.Location = new System.Drawing.Point(1100, 478);
+            this.label10.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(71, 19);
+            this.label10.Size = new System.Drawing.Size(105, 29);
             this.label10.TabIndex = 24;
             this.label10.Text = "Password";
             // 
             // txtAddress
             // 
             this.txtAddress.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.txtAddress.Location = new System.Drawing.Point(160, 366);
+            this.txtAddress.Location = new System.Drawing.Point(240, 563);
+            this.txtAddress.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtAddress.Name = "txtAddress";
-            this.txtAddress.Size = new System.Drawing.Size(397, 27);
+            this.txtAddress.Size = new System.Drawing.Size(594, 37);
             this.txtAddress.TabIndex = 26;
             // 
             // txtCity
             // 
             this.txtCity.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.txtCity.Location = new System.Drawing.Point(814, 149);
+            this.txtCity.Location = new System.Drawing.Point(1221, 229);
+            this.txtCity.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtCity.Name = "txtCity";
-            this.txtCity.Size = new System.Drawing.Size(397, 27);
+            this.txtCity.Size = new System.Drawing.Size(594, 37);
             this.txtCity.TabIndex = 27;
             // 
             // txtState
             // 
             this.txtState.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.txtState.Location = new System.Drawing.Point(814, 196);
+            this.txtState.Location = new System.Drawing.Point(1221, 302);
+            this.txtState.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtState.Name = "txtState";
-            this.txtState.Size = new System.Drawing.Size(397, 27);
+            this.txtState.Size = new System.Drawing.Size(594, 37);
             this.txtState.TabIndex = 28;
             // 
             // txtUsername
             // 
             this.txtUsername.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.txtUsername.Location = new System.Drawing.Point(814, 248);
+            this.txtUsername.Location = new System.Drawing.Point(1221, 382);
+            this.txtUsername.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtUsername.Name = "txtUsername";
-            this.txtUsername.Size = new System.Drawing.Size(397, 27);
+            this.txtUsername.Size = new System.Drawing.Size(594, 37);
             this.txtUsername.TabIndex = 29;
             // 
             // txtPassword
             // 
             this.txtPassword.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.txtPassword.Location = new System.Drawing.Point(814, 303);
+            this.txtPassword.Location = new System.Drawing.Point(1221, 466);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtPassword.Name = "txtPassword";
-            this.txtPassword.Size = new System.Drawing.Size(397, 27);
+            this.txtPassword.Size = new System.Drawing.Size(594, 37);
             this.txtPassword.TabIndex = 30;
             // 
             // txtGender
@@ -258,17 +279,18 @@ namespace GatePassGenerator
             "Male",
             "Female",
             "Other"});
-            this.txtGender.Location = new System.Drawing.Point(160, 311);
+            this.txtGender.Location = new System.Drawing.Point(240, 478);
+            this.txtGender.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtGender.Name = "txtGender";
-            this.txtGender.Size = new System.Drawing.Size(397, 27);
+            this.txtGender.Size = new System.Drawing.Size(594, 37);
             this.txtGender.TabIndex = 31;
             // 
             // AddEmploye
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1342, 536);
+            this.ClientSize = new System.Drawing.Size(2013, 825);
             this.Controls.Add(this.txtGender);
             this.Controls.Add(this.txtPassword);
             this.Controls.Add(this.txtUsername);
@@ -291,6 +313,7 @@ namespace GatePassGenerator
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "AddEmploye";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "AddEmploye";
